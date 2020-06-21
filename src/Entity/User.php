1c5058f3f0e25f78c2c07f88e4b6eb8421c84ecf
Jonathan <jonathan.laubron@gmail.com>
2020-06-21 14:16:12 +0200

    feat: user manager feature is completed

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,13 +2,24 @@
 
 namespace App\Entity;
 
-use App\Repository\UserRepository;
+use DateTimeInterface;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\UserRepository;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(
+ *     fields={"username", "email"},
+ *     errorPath="username",
+ *     message="Le pseudo que vous avez choisi existe déjà!",
+ *     errorPath="email",
+ *     message="Cette adresse email est déjà utilisée!"
+ * )
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id()
@@ -18,24 +29,44 @@ class User
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=25)
+     * @Assert\Regex("/^[\wéèàçîôûê]{2}[\w-éèàçîôûê]{0,23}$/",
+     * message="Le format du login est invalide ! Il doit comporter entre 2 et 25 caractères. La ponctuation et les caractères spéciaux sont exclus")
      */
     private $username;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Email(
+     *     message = "The email '{{ value }}' is not a valid email."
+     * )
      */
     private $email;
 
-    /**
+   /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Regex("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,}$/",
+     * message="Le mot de passe doit comporter au moins 10 caractères, posséder au moins un chiffre, une lettre majuscule et minuscule et l'un des caractères spéciaux suivants: @$!%*?&.")
      */
     private $password;
 
     /**
+     * @Assert\EqualTo(
+     *     propertyPath="password", message="Vous n'avez pas saisi le même mot de passe!"
+     * )
+     */
+    public $confirm_password;
+
+    /**
      * @ORM\Column(type="datetime")
+     * @Assert\DateTime
      */
     private $subscribeAt;
+
+     /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = ['ROLE_USER'];
 
     public function getId(): ?int
     {
@@ -88,5 +119,13 @@ class User
         $this->subscribeAt = $subscribeAt;
 
         return $this;
+    }
+
+    public function eraseCredentials() {}
+
+    public function getSalt() {}
+
+    public function getRoles() {
+        return $this->roles;
     }
 }
